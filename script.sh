26186d077f7b56d4f743fa6af3455f6339e3314f
Phil <12659478+TheRealHypR@users.noreply.github.com>
2021-05-01 21:26:17 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# paste deezer playlist, album, artist URL:
+# example:
+#deemix --portable https://www.deezer.com/de/playlist/1857061922
+
+# if spotify support is set up you can also paste a spotify uri
+# example:
+#deemix --portable spotify:playlist:37i9dQZEVXbMDoHDwVN2tF
